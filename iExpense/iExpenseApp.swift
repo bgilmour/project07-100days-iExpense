1f962d920154c6001afd9d9d2d997c92a1db716d
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-10 22:08:02 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Bruce Gilmour on 2021-07-10.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
