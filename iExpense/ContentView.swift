20bbf398d411881b537283aea666e7da96633f39
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-10 23:19:28 +0100

    Introducing sheets and @Environment

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,20 +7,25 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
 struct ContentView: View {
-    @ObservedObject var user = User()
+    @State private var showingSheet = false
 
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
+        Button("Show sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView()
+        }
+    }
+}
 
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        Button("Dismiss") {
+            presentationMode.wrappedValue.dismiss()
         }
     }
 }
